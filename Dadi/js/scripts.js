ccabbeb484eadd13288953966cdd9bfee5c38bbc
riccardotrezzi <riccardo.trezzi00@gmail.com>
2024-07-20 19:03:17 +0200

    Sistemato esercizio Dadi -Completato Esercizio

--- Dadi/js/scripts.js
+++ Dadi/js/scripts.js
@@ -1,4 +1,22 @@
 /*Dadi*/
 
+// Genero un numero random per Utente
+const userNumber = Math.floor(Math.random()* (6 - 1 + 1)) + 1;
+console.log('userNumber', userNumber, typeof userNumber);
 
 
+// Genero un numero random per PC
+const pcNumber = Math.floor(Math.random()* (6 - 1 + 1)) + 1;
+console.log('pcNumber', pcNumber, typeof pcNumber);
+
+// Controllare chi ha vinto
+
+if (userNumber > pcNumber) {
+    alert('Hai vinto!');
+}
+else if (userNumber < pcNumber) {
+    alert('Hai perso:(');
+}
+else{
+    alert('Pareggio!');
+}
